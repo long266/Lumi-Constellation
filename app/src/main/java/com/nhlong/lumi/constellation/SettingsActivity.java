b98de3baf9835619220e2e82f399da7fcce8ca10
long266 <longdangcap17@gmail.com>
2022-06-01 20:49:26 +0700

    Implement Dark Mode [WIP]

--- app/src/main/java/com/nhlong/lumi/constellation/SettingsActivity.java
+++ app/src/main/java/com/nhlong/lumi/constellation/SettingsActivity.java
@@ -1,15 +1,20 @@
 package com.nhlong.lumi.constellation;
 
-import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.preference.PreferenceManager;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener,
+        Preference.SummaryProvider<androidx.preference.ListPreference> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +30,13 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .registerOnSharedPreferenceChangeListener(this);
+
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
-
-		private Context mContext;
-		
-        @Override
-        public void onViewCreated(View view, Bundle savedInstanceState) {
-            super.onViewCreated(view, savedInstanceState);
-            getView().setBackgroundColor(getResources().getColor(R.color.hoanglong_sesl_background_color));
-        }
 
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -43,4 +44,40 @@ public class SettingsActivity extends AppCompatActivity {
 
         }
     }
+	
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        String darkModeString = getString(R.string.lumi_dark_mode);
+        if (key != null && sharedPreferences != null)
+            if (key.equals(darkModeString)) {
+                final String[] darkModeValues = getResources().getStringArray(R.array.lumi_dark_mode_values);
+                // The apps theme is decided depending upon the saved preferences on app startup
+                String pref = PreferenceManager.getDefaultSharedPreferences(this)
+                        .getString(getString(R.string.lumi_dark_mode), getString(R.string.lumi_dark_mode_def_value));
+                // Comparing to see which preference is selected and applying those theme settings
+                if (pref.equals(darkModeValues[0]))
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                if (pref.equals(darkModeValues[1]))
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                if (pref.equals(darkModeValues[2]))
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+    }
+
+    @Override
+    public CharSequence provideSummary(ListPreference preference) {
+        String key = preference.getKey();
+        if (key != null)
+            if (key.equals(getString(R.string.lumi_dark_mode)))
+                return preference.getEntry();
+        return null;
+    }
+	
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
 }
